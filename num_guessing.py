d2396926cd5707c571bc91b0f770c08292d07d59
Eva Liao <evaliaotw@gmail.com>
2020-09-08 00:44:40 +0800

    add random_start&end/count

--- num_guessing.py
+++ num_guessing.py
@@ -1,12 +1,21 @@
 import random
-r = random.randint(1,100)
+start = input('請決定隨機數字範圍開始值: ')
+end = input('請決定隨機數字範圍結束值: ')
+start = int(start)
+end = int(end)
+
+r = random.randint(start,end)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	num = input('請猜猜看，輸入一個數字: ')
 	num = int(num)
 	if num == r:
-		print('終於猜對了!')
+		print('你猜中了!')
+		print('這是你猜的第', count, '次')
 		break
 	elif num > r:
 		print('比答案大喔!')
 	else:
-		print('比答案小喔!')	
+		print('比答案小喔!')
+	print('這是你猜的第', count, '次')	
